5fe3505c2c322f438da9d45cb2a17cf026a3b1f9
abhishek-kaith <abhishekkaith76@gmail.com>
2025-03-10 16:30:18 +0530

    hypr applicatoins

---
+++ .local/scripts/install-hyprland.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+# sudo pacman -S hyprland waybar wl-clipboard wofi brightnessctl playerctl wireplumber pamixer pavucontrol xdg-desktop-portal-hyprland xdg-desktop-portal-gtk mako uwsm network-manager-applet polkit-gnome blueman udiskie
+# sudo pacman -S dmenu nemo
+# sudo pacman -S ttf-cascadia-mono-nerd ttf-cascadia-code-nerd
+# sudo pacman -S pavucontrol tela-circle-icon-theme-pink
+# yay -S bibata-cursor-theme
+
+sudo pacman -S hyprland waybar wofi hyprpaper hyprpicker wl-clipboard hypridle hyprlock hyprpolkitagent
+sudo pacman -S cliphist wl-clipboard wofi #Clipboard Manager
+sudo pacman -S pipewire wireplumber playerctl pamixer # not pipewire media sessionon
+sudo pacman -S brightnessctl # Control Brightness of display
+sudo pacman -S xdg-desktop-portal-hyprland # Screen Sharing
+sudo pacman -S network-manager-applet polkit-gnome blueman udiskie # Applets
+sudo pacman -S nautilus gvfs gvfs-mtp # FileManager
