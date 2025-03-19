2ad3ea3f42c950be1cdc260c91d5ca4d54a4408c
abhishek-kaith <abhishekkaith76@gmail.com>
2025-03-20 04:04:40 +0530

    waybar

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,12 @@
+return {
+	"ThePrimeagen/harpoon",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		vim.keymap.set("n", "<leader>a", function()
+			require("harpoon.mark").add_file()
+		end)
+		vim.keymap.set("n", "<C-e>", function()
+			require("harpoon.ui").toggle_quick_menu()
+		end)
+	end,
+}
