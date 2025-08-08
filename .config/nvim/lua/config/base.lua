bee6616a33acefa0a61f7a956ebdff3edd680490
abhishek-kaith <abhishekkaith76@gmail.com>
2025-08-08 20:27:15 +0530

    new nvim config

--- .config/nvim/lua/config/base.lua
+++ .config/nvim/lua/config/base.lua
@@ -1,40 +1,40 @@
 -- Interface Settings
-vim.opt.number = true -- Show absolute line numbers
+vim.opt.number = true         -- Show absolute line numbers
 vim.opt.relativenumber = true -- Show relative line numbers
-vim.opt.cursorline = true -- Highlight current line
-vim.wo.signcolumn = "yes" -- Always show signcolumn (for Git, LSP, etc.)
-vim.opt.wrap = false -- Don't wrap long lines
-vim.opt.scrolloff = 10 -- Minimum lines above/below cursor
-vim.opt.colorcolumn = "80"       -- Style Guide Vertical Line to guide lenght of code
+vim.opt.cursorline = true     -- Highlight current line
+vim.wo.signcolumn = "yes"     -- Always show signcolumn (for Git, LSP, etc.)
+vim.opt.wrap = false          -- Don't wrap long lines
+vim.opt.scrolloff = 10        -- Minimum lines above/below cursor
+vim.opt.colorcolumn = "80"    -- Style Guide Vertical Line to guide lenght of code
 
 -- Visuals & Characters
 vim.o.termguicolors = true -- Enable full RGB color support
 vim.opt.list = true -- Show invisible characters
 vim.opt.listchars = {
-tab = "» ", -- Show tabs as »
-trail = "·", -- Show trailing spaces
-nbsp = "␣", -- Show non-breaking space
+  tab = "» ", -- Show tabs as »
+  trail = "·", -- Show trailing spaces
+  nbsp = "␣", -- Show non-breaking space
 }
 
 -- Tabs & Indentation
-vim.o.tabstop = 2 -- Tab character = 2 spaces (visually)
-vim.o.expandtab = true -- Pressing <Tab> inserts spaces
-vim.o.softtabstop = 2 -- Tab key = 2 spaces
-vim.o.shiftwidth = 2 -- Indentation = 2 spaces
+vim.o.tabstop = 2        -- Tab character = 2 spaces (visually)
+vim.o.expandtab = true   -- Pressing <Tab> inserts spaces
+vim.o.softtabstop = 2    -- Tab key = 2 spaces
+vim.o.shiftwidth = 2     -- Indentation = 2 spaces
 vim.o.breakindent = true -- Indent wrapped lines properly
 
 -- Search Behavior
-vim.o.hlsearch = true -- Don't highlight matches by default
+vim.o.hlsearch = true   -- Don't highlight matches by default
 vim.o.ignorecase = true -- Ignore case when searching...
-vim.o.smartcase = true -- ...unless capital letters are used
+vim.o.smartcase = true  -- ...unless capital letters are used
 
 -- Clipboard & Undo
 vim.o.clipboard = "unnamedplus" -- Use system clipboard (works with Ctrl+C / Ctrl+V)
-vim.o.undofile = true -- Save undo history to disk
-vim.o.swapfile = false -- Disable swap file
+vim.o.undofile = true           -- Save undo history to disk
+vim.o.swapfile = false          -- Disable swap file
 
 -- Mouse & Splits
-vim.o.mouse = "a" -- Enable mouse in all modes
+vim.o.mouse = "a"         -- Enable mouse in all modes
 vim.opt.splitright = true -- Vertical splits open to the right
 vim.opt.splitbelow = true -- Horizontal splits open below
 
@@ -48,11 +48,94 @@ vim.opt.splitbelow = true -- Horizontal splits open below
 
 -- Plugins
 vim.pack.add({
-  "https://github.com/vague2k/vague.nvim",
-  "https://github.com/ibhagwan/fzf-lua"
+  -- theme
+  { src = "https://github.com/vague2k/vague.nvim" },
+  -- fuzzy finder
+  { src = "https://github.com/ibhagwan/fzf-lua" },
+  -- lsp
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim" },
+  { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+  { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+  -- autocomplete with snippets
+  { src = "https://github.com/Saghen/blink.cmp", version="v1.6.0" },
+  { src = "https://github.com/L3MON4D3/LuaSnip" },
+  { src = "https://github.com/rafamadriz/friendly-snippets" },
+  -- Syntax highlight
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
 
-require("fzf-lua").setup({'fzf-native'})
+require("fzf-lua").setup({ 'fzf-native' })
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+  ensure_installed = {
+    "lua_ls",
+    "stylua",
+    "ts_ls",
+    "tailwindcss"
+  },
+})
+
+vim.lsp.config("lua_ls", {
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = {
+          "vim",
+          "require",
+        },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
+require("luasnip.loaders.from_vscode").lazy_load()
+-- Use C f and C b to read docs
+require("blink.cmp").setup({
+  signature = {
+    enabled = true
+  },
+  completion = {
+    documentation = { auto_show = true, auto_show_delay_ms = 500 },
+    menu = {
+      auto_show = true,
+      draw = {
+        treesitter = { "lsp" },
+        columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+      },
+    },
+  },
+})
+
+require 'nvim-treesitter.configs'.setup {
+  ensure_installed = {},
+  sync_install = false,
+  auto_install = true,
+  ignore_install = {},
+  modules = {},
+  highlight = {
+    enable = true,
+    ---@diagnostic disable-next-line: unused-local
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+    additional_vim_regex_highlighting = false,
+  },
+}
